6d08861c1c973fa03c371014388b6d553f22272a
Dimitris Panokostas <midwan@gmail.com>
2015-08-26 21:41:02 +0200

    initial commit

---
+++ RetroRomer/FileReader.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RetroRomer
+{
+    public class FileReader
+    {
+        public IEnumerable<string> ReadFile(string file)
+        {
+            if (string.IsNullOrEmpty(file) || string.IsNullOrWhiteSpace(file)) return new List<string>();
+            if (!System.IO.File.Exists(file)) return new List<string>();
+            try
+            {
+                var contents = System.IO.File.ReadAllLines(file);
+                var contentsToList = new List<string>(contents);
+                return contentsToList;
+            }
+            catch (Exception)
+            {
+                return new List<string>();
+            }
+        }
+
+        public IEnumerable<string> ProcessEntries(IEnumerable<string> fileContents)
+        {
+            var modifiedList = fileContents.Select(entry => $"{entry}.zip").ToList();
+            return modifiedList;
+        }
+    }
+}
---
+++ RetroRomerTest/FileReaderTest.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using RetroRomer;
+
+namespace RetroRomerTest
+{
+    [TestClass]
+    public class FileReaderTest
+    {
+        private FileReader _fileReader;
+
+        [TestInitialize]
+        public void Initialize()
+        {
+            _fileReader = new FileReader();
+        }
+
+        [TestMethod]
+        public void TestReadFile()
+        {
+            var file = @"D:\Downloads\MAMEUI_miss.txt";
+
+            var result = _fileReader.ReadFile(file);
+
+            Assert.IsNotNull(result);
+            Assert.IsInstanceOfType(result, typeof(List<string>));
+            Assert.IsTrue(result.Any());
+        }
+
+        [TestMethod]
+        public void TestProcessContents()
+        {
+            var contents = new List<string>
+            {
+                "entry1",
+                "entry2",
+                "entry3"
+            };
+
+            var result = _fileReader.ProcessEntries(contents);
+
+            Assert.IsNotNull(result);
+            Assert.IsInstanceOfType(result, typeof(List<string>));
+            Assert.AreEqual("entry1.zip", result.First());
+        }
+    }
+}

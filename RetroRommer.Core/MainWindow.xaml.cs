e865313828eb7aa57b2f9aeb406538a0b3aa47d7
Dimitris Panokostas <midwan@gmail.com>
2020-10-13 16:39:26 +0200

    Rebuilt whole project using NETCore 3.1

---
+++ RetroRommer.Core/MainWindow.xaml.cs
@@ -0,0 +1,89 @@
+﻿using System.IO;
+using System.Windows;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Win32;
+using Ookii.Dialogs.Wpf;
+using RetroRommer.Domain;
+using Serilog;
+using Serilog.Core;
+
+namespace RetroRommer.Core
+{
+    /// <summary>
+    ///     Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private readonly RetroRommerService _service;
+        private string _destinationPath;
+        private string _filename;
+        private readonly Logger _logger;
+        private string _password;
+        private string _username;
+
+        public MainWindow()
+        {
+            var configuration = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json")
+                .Build();
+
+            _logger = new LoggerConfiguration()
+                .ReadFrom.Configuration(configuration)
+                .CreateLogger();
+
+            InitializeComponent();
+
+            _service = new RetroRommerService(_logger);
+
+            TextBoxCustomWebsite.Text = configuration.GetValue("Website", string.Empty);
+            TextBoxFilename.Text = configuration.GetValue("MissFile", string.Empty);
+            TextBoxDestination.Text = configuration.GetValue("Destination", string.Empty);
+            TextBoxUsername.Text = configuration.GetValue("Username", string.Empty);
+            PBoxPassword.Password = configuration.GetValue("Password", string.Empty);
+        }
+
+        private void ButtonSelectFile_Click(object sender, RoutedEventArgs e)
+        {
+            var dlg = new OpenFileDialog
+            {
+                DefaultExt = ".txt",
+                Filter =
+                    "TXT Files (*.txt)|*.txt"
+            };
+
+            var result = dlg.ShowDialog();
+            if (result != true) return;
+            var filename = dlg.FileName;
+            TextBoxFilename.Text = filename;
+        }
+
+        private void ButtonSelectDestination_Click(object sender, RoutedEventArgs e)
+        {
+            var dialog = new VistaFolderBrowserDialog();
+            if (dialog.ShowDialog() == true) TextBoxDestination.Text = dialog.SelectedPath;
+        }
+
+        private void ButtonDownload_Click(object sender, RoutedEventArgs e)
+        {
+            _filename = TextBoxFilename.Text;
+            _destinationPath = TextBoxDestination.Text;
+            _username = TextBoxUsername.Text;
+            _password = PBoxPassword.Password;
+
+            PrepareAndDownloadFiles();
+
+            MessageBox.Show("Download finished!", "Operation completed", MessageBoxButton.OK);
+        }
+
+        private async void PrepareAndDownloadFiles()
+        {
+            _logger.Information("Beginning to download files...");
+            var fileContents = _service.ReadFile(_filename);
+            var processedContents = _service.AddFilenameExtensionToEntries(fileContents);
+
+            foreach (var file in processedContents)
+                await _service.GetFile(file, _username, _password, _destinationPath);
+        }
+    }
+}
---
+++ RetroRommer.Domain/RetroRommerService.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Threading.Tasks;
+using Serilog;
+
+namespace RetroRommer.Domain
+{
+    public class RetroRommerService
+    {
+        private readonly ILogger _logger;
+
+        public RetroRommerService(ILogger logger)
+        {
+            _logger = logger;
+        }
+
+        public bool WriteFile(string filename, IEnumerable<string> fileContents)
+        {
+            if (string.IsNullOrEmpty(filename) || string.IsNullOrWhiteSpace(filename)) return false;
+            try
+            {
+                File.WriteAllLines(filename, fileContents);
+            }
+            catch (Exception)
+            {
+                return false;
+            }
+
+            return true;
+        }
+
+        public IEnumerable<string> ReadFile(string file)
+        {
+            if (string.IsNullOrEmpty(file) || string.IsNullOrWhiteSpace(file)) return new List<string>();
+            if (!File.Exists(file)) return new List<string>();
+            try
+            {
+                var contents = File.ReadAllLines(file);
+                var contentsToList = new List<string>(contents);
+                return contentsToList;
+            }
+            catch (Exception e)
+            {
+                // ReSharper disable once RedundantVerbatimPrefix
+                _logger.Fatal($"Failed to read file {file}\n{@e}");
+                return new List<string>();
+            }
+        }
+
+        public IEnumerable<string> AddFilenameExtensionToEntries(IEnumerable<string> fileContents)
+        {
+            var modifiedList = new List<string>();
+            foreach (var entry in fileContents)
+                if (!string.IsNullOrWhiteSpace(entry) && !entry.Contains("You are missing"))
+                    modifiedList.Add($"{entry}.zip");
+            return modifiedList;
+        }
+
+        public async Task<bool> GetFile(string file, string userName, string passwd, string destination)
+        {
+            var website = $"https://bda.retroroms.info:82/downloads/mame/currentroms/{file}";
+            using var client = new HttpClient();
+
+            var authToken = Encoding.ASCII.GetBytes($"{userName}:{passwd}");
+            client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Basic",
+                Convert.ToBase64String(authToken));
+
+            try
+            {
+                var fileBytes = await client.GetByteArrayAsync(website);
+                var localPath = Path.Combine(destination, file);
+                _logger.Information($"Downloading file from {website} to {localPath}");
+                await File.WriteAllBytesAsync(localPath, fileBytes);
+            }
+            catch (Exception ex)
+            {
+                // ReSharper disable once RedundantVerbatimPrefix
+                _logger.Fatal($"File failed to download: {file}.\n{@ex}");
+                return false;
+            }
+
+            return true;
+        }
+    }
+}
